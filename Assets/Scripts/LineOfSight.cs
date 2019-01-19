c229f4184ce594f828dfb5fcdfbcc5863210b681
Justin Lesh <jlesh56021@gmail.com>
2019-01-19 17:18:51 +0000

    added line of sight script and attached to receptionist for testing

---
+++ Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour {
+    public GameObject player;
+    MeshRenderer mesh;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+    void Update()
+    {
+        //Debug.Log("Testing");
+        if (CanSeePlayer(player))
+        {
+            mesh.material.color = Color.red;
+        }
+        else
+        {
+            mesh.material.color = Color.blue;
+        }
+    }
+
+    bool CanSeePlayer(GameObject target)
+    {
+        //float heightOfPlayer = 1.5f;
+
+        Vector3 startVec = transform.position;
+        //startVec.y += heightOfPlayer;
+        Vector3 startVecFwd = transform.forward;
+        //startVecFwd.y += heightOfPlayer;
+
+        RaycastHit hit;
+        Vector3 rayDirection = target.transform.position - startVec;
+        Debug.DrawRay(startVec, rayDirection, Color.yellow);
+        // If the ObjectToSee is close to this object and is in front of it, then return true
+        if ((Vector3.Angle(rayDirection, startVec)) < 30 &&
+            (Vector3.Distance(startVec, target.transform.position) <= 20f))
+        {
+            //Debug.Log("close");
+            return true;
+        }
+        if ((Vector3.Angle(rayDirection, startVecFwd)) < 30 &&
+            Physics.Raycast(startVec, rayDirection, out hit, 5f))
+        { // Detect if player is within the field of view
+
+            if (hit.collider.gameObject == target)
+            {
+                Debug.Log("Can see player");
+                return true;
+            }
+            else
+            {
+                Debug.Log("Can not see player");
+                return false;
+            }
+        }
+        return false;
+    }
+}
