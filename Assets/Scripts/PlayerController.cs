5237eb1affc34fd044051a06e087f28022d93774
Caleb Corbin <corbin_caleb@columbusstate.edu>
2019-01-19 02:37:59 +0000

    No Comments

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float walkSpeed;
+
+    Rigidbody rb;
+    Vector3 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+        moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = moveDirection * walkSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+}
