18d1fa10a6f6ec9dffe5e381510cc23b9ef811d5
Caleb Corbin <corbin_caleb@columbusstate.edu>
2019-01-19 19:33:02 +0000

    AI script but it has a null reference in update

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AI : MonoBehaviour {
 
+    public Transform[] path;
+    public float speed = 5.0f;
+    public float reachDistance = 1.0f;
+    public int currentPoint = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,33 @@ public class AI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        float distance = Vector3.Distance(path[currentPoint].position, transform.position);
+
+        transform.position = Vector3.Lerp(transform.position, path[currentPoint].position, Time.deltaTime * speed);
+
+        if (distance <= reachDistance)
+        {
+            currentPoint++;
+        }
+
+        if (currentPoint >= path.Length)
+        {
+            currentPoint = 0;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path.Length > 0)
+        {
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] != null)
+                {
+                    Gizmos.DrawSphere(path[i].position, reachDistance);
+                }
+            }
+        }
+    }
 }
