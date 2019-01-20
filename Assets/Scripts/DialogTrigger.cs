2c2dab579d6c00e6eac9798849d8f3efe9fc56ee
Justin Lesh <jlesh56021@gmail.com>
2019-01-20 20:05:16 +0000

    No Comments

---
+++ Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour {
+    public GameObject dialogsys;
+    private bool triggeredAlready = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player") && !triggeredAlready)
+        {
+            dialogsys.SetActive(true);
+        }
+    }
+}
