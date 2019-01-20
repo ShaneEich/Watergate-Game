467aef956a5366854b2ce795fa9e43da7e21ed39
Caleb Corbin <corbin_caleb@columbusstate.edu>
2019-01-20 16:19:36 +0000

    No Comments

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -53,59 +53,75 @@ public class PlayerInteract : MonoBehaviour {
 
             if (currentInterObjScript.openDoor && keyCards.Count > 0)
             {
-                    currentInterObjScript.open();
-                    Debug.Log("Door is open");
-                }
-            
-            
-                if (currentInterObjScript.closeDoor && keyCards.Count > 0)
-                {
-                    currentInterObjScript.close();
-                    keyCards.Remove(keyCard);
-                    Debug.Log("Door is closed");
-                    Scene scene = SceneManager.GetActiveScene();
-                    SceneManager.LoadScene(scene.buildIndex + 1);
-                }
-              /* if(currentCombatScript.isGuard && weapons.Count > 0)
+                currentInterObjScript.open();
+                Debug.Log("Door is open");
+            }
+
+
+            if (currentInterObjScript.closeDoor && keyCards.Count > 0)
             {
-                currentCombatScript.KOGuard();
-                weapons.Remove(weapon);
-                Debug.Log("Guard Knocked Out");
-            }*/
+                currentInterObjScript.close();
+                keyCards.Remove(keyCard);
+                Debug.Log("Door is closed");
+                SceneManager.LoadScene("Level1");
+            }
+            /* if(currentCombatScript.isGuard && weapons.Count > 0)
+          {
+              currentCombatScript.KOGuard();
+              weapons.Remove(weapon);
+              Debug.Log("Guard Knocked Out");
+          }*/
 
         }
-
+        if (Input.GetButtonDown("Attack") && currentEnemy)
+        {
+            if (weapons.Count > 0)
+            {
+                currentEnemy.SetActive(false);
+                weapons.Remove(weapon);
+                Debug.Log("Guard Knocked Out");
+            }else if(weapons.Count <= 0)
+            {
+                Debug.Log("No weapons available.");
+            }
+        }
     }
 
     //check if the player is colliding with a trigger and display the trigger's name
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider collider)
     {
         // Check if in trigger of object and collect objects script
-        if (other.CompareTag("Interactable Object"))
+        if (collider.CompareTag("Interactable Object"))
         {
-            Debug.Log(other.name);
+            Debug.Log(collider.name);
 
-            currentInterObj = other.gameObject;
+            currentInterObj = collider.gameObject;
             currentInterObjScript = currentInterObj.GetComponent<ObjectInteract>();
         }
-        /*if (other.CompareTag("Guard"))
+        if (collider.CompareTag("Guard"))
         {
-            Debug.Log(other.name);
-            currentEnemy = other.gameObject;
-            currentCombatScript = currentEnemy.GetComponent<AICombat>();
+            Debug.Log(collider.name);
+            currentEnemy = collider.gameObject;
+            //currentCombatScript = currentEnemy.GetComponent<AICombat>();
 
-        }*/
+        }
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider collider)
     {
-        if (other.CompareTag("Interactable Object"))
+        if (collider.CompareTag("Interactable Object"))
         {
-            if (other.gameObject == currentInterObj)
+            if (collider.gameObject == currentInterObj)
             {
                 currentInterObj = null;
             }
 
+        }else if (collider.CompareTag("Guard"))
+        {
+            if(collider.gameObject == currentEnemy)
+            {
+                currentEnemy = null;
+            }
         }
     }
 }
