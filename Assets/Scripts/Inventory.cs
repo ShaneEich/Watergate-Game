6d24e86b9efe4022ba011656083bd024015ee7a3
Caleb Corbin <corbin_caleb@columbusstate.edu>
2019-03-29 17:40:16 +0000

    You can set inventory to inactive with I and O should open set it to active but isn't. Not sure why, don't wanna bother rn

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            //set inventory GUI to inactive
+            gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            //set inventory GUI to active
+            gameObject.SetActive(true);
+        }
+    }
+}
