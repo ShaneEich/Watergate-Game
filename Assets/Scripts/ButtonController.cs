fcbce73e3fc7002a655147df4bd7242c4ab6b817
Justin Lesh <jlesh56021@gmail.com>
2019-04-09 16:55:34 +0000

    how to play scene and changed way buttons work in menu and howtoplay

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum FUNC {START, HOWTO, QUIT}
+public class ButtonController : MonoBehaviour
+{
+    public FUNC state;
+    public void OnClick()
+    {
+        if (state == FUNC.START)
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+        else if (state == FUNC.HOWTO)
+        {
+            SceneManager.LoadScene("HowToPlay");
+        }
+        else if (state == FUNC.QUIT)
+        {
+            Application.Quit();
+        }
+    }
+}
