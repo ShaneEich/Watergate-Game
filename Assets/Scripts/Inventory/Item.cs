967625e5d24d63adb898f6c04da9d8deb2e63a48
Caleb Corbin <corbin_caleb@columbusstate.edu>
2019-04-09 12:49:33 +0000

    nvm it needs to stay mono behaviour

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 /* The base item class. All items should derive from this. */
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
-public class Item : ScriptableObject
+public class Item : MonoBehaviour
 {
 
 	new public string name = "New Item";	// Name of the item
